cb61ba531113e1b191640c69c3c88f5d40a72ac0
Ahmad Jafar <ahmadzafar100@gmail.com>
2026-02-23 10:25:28 +0530

    add active class in menu

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -3,25 +3,25 @@
         Pages
     </li>
 
-    <li class="sidebar-item active">
-        <a class="sidebar-link" href="{{ url('/') }}">
+    <li class="sidebar-item {{ request()->is('admin/dashboard') ? 'active' : '' }}">
+        <a class="sidebar-link" href="{{ url('/admin/dashboard') }}">
             <i class="align-middle" data-feather="chevron-right"></i> <span class="align-middle">Dashboard</span>
         </a>
     </li>
 
-    <li class="sidebar-item">
+    <li class="sidebar-item {{ request()->is('admin/category') ? 'active' : '' }}">
         <a class="sidebar-link" href="{{ url('/admin/category') }}">
             <i class="align-middle" data-feather="chevron-right"></i> <span class="align-middle">Category</span>
         </a>
     </li>
 
-    <li class="sidebar-item">
+    <li class="sidebar-item {{ request()->is('admin/subcategory') ? 'active' : '' }}">
         <a class="sidebar-link" href="{{ url('/admin/subcategory') }}">
             <i class="align-middle" data-feather="chevron-right"></i> <span class="align-middle">Subcategory</span>
         </a>
     </li>
 
-    <li class="sidebar-item">
+    <li class="sidebar-item {{ request()->is('admin/news') ? 'active' : '' }}">
         <a class="sidebar-link" href="{{ url('/admin/news') }}">
             <i class="align-middle" data-feather="chevron-right"></i> <span class="align-middle">News</span>
         </a>
